b2b0da98821f63f4c64298a28acb24f55398c03a
aaroncolesee <aaron.see@obf.ateneo.edu>
2019-04-29 13:32:46 +0800

    added variables to gui

--- SARndbox-2.4/GUI.java
+++ SARndbox-2.4/GUI.java
@@ -40,30 +40,36 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 		
 		JPanel centre = new JPanel();
 		centre.setLayout(new BoxLayout(centre, BoxLayout.Y_AXIS));
-		sliders = new JSlider[4];
+		sliders = new JSlider[6];
 		//values must be int; convert to multiples of 100
 		sliders[0] = new JSlider(JSlider.HORIZONTAL, 0, 50, 25); //rain strength 0.25
-		sliders[1] = new JSlider(JSlider.HORIZONTAL, 0, 200, 100); //water speed 1.0
-		sliders[2] = new JSlider(JSlider.HORIZONTAL, 0, 60, 30); //water steps 30
-		sliders[3] = new JSlider(JSlider.HORIZONTAL, 0, 100, 50); //evaporation rate 0.0
-		JLabel[] labels = new JLabel[4];
+		sliders[1] = new JSlider(JSlider.HORIZONTAL, 0, 100, 50); //evaporation rate 0.0
+		sliders[2] = new JSlider(JSlider.HORIZONTAL, 0, 200, 100); //water speed 1.0
+		sliders[3] = new JSlider(JSlider.HORIZONTAL, 0, 400, 200); //water opacity 2.0
+		sliders[4] = new JSlider(JSlider.HORIZONTAL, 0, 150, 75); //contour lines 0.75
+		sliders[5] = new JSlider(JSlider.HORIZONTAL, 0, 0, 0); //placeholder
+		JLabel[] labels = new JLabel[6];
 		labels[0] = new JLabel("Rain Strength");
-		labels[1] = new JLabel("Water Speed");
-		labels[2] = new JLabel("Water Steps");
-		labels[3] = new JLabel("Evaporation Rate");
+		labels[1] = new JLabel("Evaporataion Rate");
+		labels[2] = new JLabel("Water Speed");
+		labels[3] = new JLabel("Water Opacity");
+		labels[4] = new JLabel("Contour Lines");
+		labels[5] = new JLabel("Scale   1:");
 		JPanel panel;
-		textField = new JTextField[4];
+		textField = new JTextField[6];
 		textField[0] = new JTextField("0.25");
-		textField[1] = new JTextField("1.0");
-		textField[2] = new JTextField("30");
-		textField[3] = new JTextField("0.0");
+		textField[1] = new JTextField("0.0");
+		textField[2] = new JTextField("1.0");
+		textField[3] = new JTextField("2.0");
+		textField[4] = new JTextField("0.75");
+		textField[5] = new JTextField("100");
 
-		for(int i = 0; i < 4; i++)
+		for(int i = 0; i < 6; i++)
 		{
 			panel = new JPanel();
 			panel.setLayout(new FlowLayout(FlowLayout.CENTER));
 			panel.add(labels[i]);
-			panel.add(sliders[i]);
+			if(i != 5) panel.add(sliders[i]);
 			panel.add(textField[i]);
 			// textField[i].addActionListener(this);
 			textField[i].setHorizontalAlignment(JTextField.CENTER);
@@ -92,11 +98,11 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 			}
 		});
 
-		// water speed
+		// evaporation rate
 		sliders[1].addChangeListener(new ChangeListener(){
 			public void stateChanged(ChangeEvent ce)
 			{
-				Double value = Double.valueOf(sliders[1].getValue())/100;
+				Double value = Double.valueOf(sliders[1].getValue()-50)/100;
 				textField[1].setText("" + value);
 			}
 		});
@@ -104,19 +110,19 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 			public void keyReleased(KeyEvent ke)
 			{
 				String typed = textField[1].getText();
-				if(!typed.matches("\\d[.]\\d+")){
+				if(!typed.matches("^[-+]?\\d[.]\\d+")){
 					return;
 				}
-				int value = (int)(Double.parseDouble(typed)*100);
-        	sliders[1].setValue(value); 
+				int value = (int)((Double.parseDouble(typed)*100)+50);
+        	sliders[1].setValue(value);
 			}
 		});
 
-		// water steps
+		// water speed
 		sliders[2].addChangeListener(new ChangeListener(){
 			public void stateChanged(ChangeEvent ce)
 			{
-				int value = sliders[2].getValue();
+				Double value = Double.valueOf(sliders[2].getValue())/100;
 				textField[2].setText("" + value);
 			}
 		});
@@ -124,19 +130,19 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 			public void keyReleased(KeyEvent ke)
 			{
 				String typed = textField[2].getText();
-				if(!typed.matches("\\d{1,2}")){
+				if(!typed.matches("\\d[.]\\d+")){
 					return;
 				}
-				int value = Integer.parseInt(typed);
+				int value = (int)(Double.parseDouble(typed)*100);
         	sliders[2].setValue(value); 
 			}
-		});
+		});		
 
-		// evaporation rate
+		// water opacity
 		sliders[3].addChangeListener(new ChangeListener(){
 			public void stateChanged(ChangeEvent ce)
 			{
-				Double value = Double.valueOf(sliders[3].getValue()-50)/100;
+				Double value = Double.valueOf(sliders[3].getValue())/100;
 				textField[3].setText("" + value);
 			}
 		});
@@ -144,26 +150,48 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 			public void keyReleased(KeyEvent ke)
 			{
 				String typed = textField[3].getText();
-				if(!typed.matches("^[-+]?\\d[.]\\d+")){
+				if(!typed.matches("\\d[.]\\d+")){
 					return;
 				}
-				int value = (int)((Double.parseDouble(typed)*100)+50);
-        	sliders[3].setValue(value);
+				int value = (int)(Double.parseDouble(typed)*100);
+        	sliders[3].setValue(value); 
+			}
+		});	
+
+		// contour lines
+		sliders[4].addChangeListener(new ChangeListener(){
+			public void stateChanged(ChangeEvent ce)
+			{
+				Double value = Double.valueOf(sliders[4].getValue())/100;
+				textField[4].setText("" + value);
 			}
 		});
+		textField[4].addKeyListener(new KeyAdapter(){
+			public void keyReleased(KeyEvent ke)
+			{
+				String typed = textField[4].getText();
+				if(!typed.matches("\\d[.]\\d+")){
+					return;
+				}
+				int value = (int)(Double.parseDouble(typed)*100);
+        	sliders[4].setValue(value); 
+			}
+		});		
 		
 		centre.add(button = new JButton("Simulate"));
 		button.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ae)
 			{
 				Double rs = Double.valueOf(sliders[0].getValue())/100;
-				Double ws1 = Double.valueOf(sliders[1].getValue())/100;
-				int ws2 = sliders[2].getValue();
-				Double evr = Double.valueOf(sliders[3].getValue())/100 - 0.5;
+				Double evr = Double.valueOf(sliders[1].getValue())/100 - 0.5;
+				Double ws1 = Double.valueOf(sliders[2].getValue())/100;
+				Double wo = Double.valueOf(sliders[3].getValue())/100;
+				Double ucl = Double.valueOf(sliders[4].getValue())/100;
+				Double s = Double.valueOf(textField[5].getText());
 				pb = new ProcessBuilder(new String[]{"/bin/bash", "-c", 
 				"make && ./bin/SARndbox -uhm -fpv -rs " + rs + 
-				" -ws " + ws1 + " " + ws2 + 
-				" -evr " + evr + " > out.txt"});
+				" -ws " + ws1 + " -evr " + evr +
+				" -wo " + wo +  " -ucl " + ucl + " > out.txt"});
 				try
 				{
 					p = pb.start();
@@ -203,20 +231,20 @@ public class GUI extends JFrame implements ActionListener, ChangeListener
 			case "Yellow":
 				value = Math.random() * 7.5 + 7.5;
 				value = value / 360;
-				textField[3].setText(value.toString());
-				sliders[3].setValue((int)(value * 100.0)+50);
+				textField[1].setText(value.toString());
+				sliders[1].setValue((int)(value * 100.0)+50);
 				break;
 			case "Orange":
 				value = Math.random() * 15 + 15;
 				value = value / 360;
-				textField[3].setText(value.toString());
-				sliders[3].setValue((int)(value * 100.0)+50);
+				textField[1].setText(value.toString());
+				sliders[1].setValue((int)(value * 100.0)+50);
 				break;
 			case "Red":
 				value = Math.random() * 15 + 30;
 				value = value / 360;
-				textField[3].setText(value.toString());
-				sliders[3].setValue((int)(value * 100.0)+50);
+				textField[1].setText(value.toString());
+				sliders[1].setValue((int)(value * 100.0)+50);
 				break;
 			default:
 				break;
